d447a8ba1607168b572c7feae9e5ccdfc9884d65
Vercaemer Eduard <vercaemereduard@gmail.com>
2022-05-10 12:05:58 -0500

    add some pandas functions

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 import pandas as pd
 
+# cargar datos
 data = pd.read_csv("Countries GDP 1960-2020.csv")
+
+# observar datos iniciales
 print(data.head())
+
+# cantidad de paises
+print("paises: ", len(data.index))
+
+# maximos, minimos, etc, de cada año
+print(data[data.columns[2:]].describe())
+
+# paises y su gdp en 2020
+print(data[["Country Name", "2020"]])
 
